487a46dbdc76c7269ece000f3d73a3bb7e58a896
Kanishk Sharma <63721678+k9kanishk@users.noreply.github.com>
2026-02-13 19:10:24 +0000

    Add Twelve Data provider and prioritize it in reconciliation

--- dq/providers/registry.py
+++ dq/providers/registry.py
@@ -2,6 +2,7 @@ from .fred import FREDProvider
 from .stooq import StooqProvider
 from .yfinance_provider import YFinanceProvider
 from .ecb_fx import ECBFXProvider, ECBFXCrossProvider
+from .twelvedata import TwelveDataProvider
 
 PROVIDERS = {
     "fred": FREDProvider(),
@@ -9,6 +10,7 @@ PROVIDERS = {
     "yfinance": YFinanceProvider(),
     "ecb_fx": ECBFXProvider(),
     "ecb_fx_cross": ECBFXCrossProvider(),
+    "twelvedata": TwelveDataProvider(),
 }
 
 def get_provider(name: str):
